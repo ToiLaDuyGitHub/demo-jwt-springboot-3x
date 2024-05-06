75891fef928463cf085fb6d8642303484a969883
Duy Vu <toiladuygg@gmail.com>
2024-05-06 16:29:20 +0700

    Create project for JWT Token in Spring Boot 3.0

---
+++ src/main/java/org/example/demojwtspringboot3x/DemoJwtSpringboot3xApplication.java
@@ -0,0 +1,13 @@
+package org.example.demojwtspringboot3x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSpringboot3xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtSpringboot3xApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demojwtspringboot3x/DemoJwtSpringboot3xApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demojwtspringboot3x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSpringboot3xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
